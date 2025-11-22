c32ddb3d808b711e68c76a91ecbae67a6f34a591
RAYALA PRABHAS <prabhas.rayala@slu.edu>
2025-11-22 22:57:26 +0000

    LAB- 5

--- api.js
+++ api.js
@@ -1,16 +1,15 @@
 const path = require('path')
 const Products = require('./products')
+const Orders = require('./orders')
 const autoCatch = require('./lib/auto-catch')
 
 /**
  * Handle the root route
  * @param {object} req
- * @param {object} res
 */
 function handleRoot(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 }
-
 /**
  * List all products
  * @param {object} req
@@ -23,11 +22,7 @@ async function listProducts(req, res) {
   res.json(await Products.list({
     offset: Number(offset),
     limit: Number(limit),
-    tag
-  }))
-}
-
-
+@@ -30,49 +31,60 @@
 /**
  * Get a single product
  * @param {object} req
@@ -35,23 +30,22 @@ async function listProducts(req, res) {
  */
 async function getProduct(req, res, next) {
   const { id } = req.params
-
   const product = await Products.get(id)
   if (!product) {
     return next()
   }
-
   return res.json(product)
 }
-
 /**
  * Create a product
  * @param {object} req 
- * @param {object} res 
- */
+ * @param {object} res
+*/
 async function createProduct(req, res) {
   console.log('request body:', req.body)
   res.json(req.body)
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
 /**
@@ -63,6 +57,11 @@ async function createProduct(req, res) {
 async function editProduct(req, res, next) {
   console.log(req.body)
   res.json(req.body)
+async function editProduct (req, res, next) {
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+
+  res.json(product)
 }
 
 /**
@@ -73,13 +72,51 @@ async function editProduct(req, res, next) {
  */
 async function deleteProduct(req, res, next) {
   res.json({ success: true })
+async function deleteProduct (req, res, next) {
+  const response = await Products.destroy(req.params.id)
+
+  res.json(response)
+}
+
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+
+  res.json(order)
+}
+
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
 }
 
 module.exports = autoCatch({
-  handleRoot,
-  listProducts,
+@@ -81,5 +93,7 @@ module.exports = autoCatch({
   getProduct,
   createProduct,
   editProduct,
   deleteProduct
+  deleteProduct,
+  listOrders,
+  createOrder
 });
+  3 changes: 3 additions & 0 deletions3  
+app.js
+Viewed
+Original file line number	Diff line number	Diff line change
+@@ -19,6 +19,9 @@ app.get('/products/:id', api.getProduct)
+app.put('/products/:id', api.editProduct)
+app.delete('/products/:id', api.deleteProduct)
+app.post('/products', api.createProduct)
+
+app.get('/orders', api.listOrders)
+app.post('/orders/', api.createOrder)
+// Boot the server
+app.listen(port, () => console.log(`Server listening on port ${port}`))
